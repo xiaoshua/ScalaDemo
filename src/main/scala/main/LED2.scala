31bdf7170c5e743171362a27d91ba9b2d938bc9f
aafri <connerlyzhang@gmail.com>
2015-06-17 15:53:33 +0800

    scala1

---
+++ src/main/scala/main/LED2.scala
@@ -0,0 +1,39 @@
+package main
+
+/**
+ * Created by Administrator on 2015/6/16.
+ */
+trait LED2 {
+  val ledBase = Array(
+    " _     _  _     _  _  _  _  _ ",
+    "|  | | _| _||_||_ |_   ||_||_|",
+    "|_| ||_  _|  | _||_|  ||_| _|"
+  )
+
+  def num2led(num: Int): String = {
+
+    val tmpList = new Array[String](3)
+    for (i <- 0 to 2) {
+      tmpList(i) = ""
+    }
+
+    for (strDigit <- ("" + num).toCharArray) {
+      val iDigit = strDigit.toInt - 48
+      for (j <- 0 to 2) {
+        for (i <- iDigit * 3 to iDigit * 3 + 2) {
+          tmpList(j) += ledBase(j).charAt(i).toString()
+        }
+      }
+    }
+
+    var result = ""
+    for (line <- tmpList) {
+      result += line + "\n"
+    }
+    result
+  }
+
+  def main(args: Array[String]) {
+    print(num2led(987))
+  }
+}
